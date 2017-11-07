c9ee50d234ecc7567a27563d0950f00083e17caa
wgg <is0332ls@ed,ritsumei.ac.jp>
2017-11-07 11:57:33 +0900

    Resourceクラス作成

---
+++ MascotBattle/src/Singleton.h
@@ -0,0 +1,19 @@
+/***** Singleton.h *****/
+// éQçl: http://dixq.net/rp2/08.html
+#pragma once
+
+template <typename _T>
+class Singleton {
+protected:
+	Singleton() {}
+	virtual ~Singleton() {}
+	Singleton(const Singleton& r) {}
+	Singleton& operator=(const Singleton& r) {}
+
+public:
+	static _T* Instance() {
+		static _T inst;
+		return &inst;
+	};
+
+};
