c605370986d12233086e781146b6489222c6fa86
wgg <is0332ls@ed,ritsumei.ac.jp>
2017-11-07 16:47:38 +0900

    起動時の初期化処理を設定,ディスプレイサイズに合わせて画面の拡大率を設定するように

--- MascotBattle/src/main.cpp
+++ MascotBattle/src/main.cpp
@@ -1,44 +1,26 @@
 #include "./DxLib.h"
 
 #include "Scene.h"
-#include "Input.h"
+#include "SystemManager.h"
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	ChangeWindowMode(TRUE);
-
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
-	{
-		return -1;			// エラーが起きたら直ちに終了
-	}
+	// 初期化処理
+	if (SystemManager::Instance()->initialize() == false)return -1;
 
 	// シーン登録
 	Scene::Instance()->push(new Title());
 
 	// メインループ 
 	// while内の処理は原則毎秒60回実行される
-	while (true) {
-		if (DxLib::ProcessMessage() == -1)break; // プロセス処理
-		if (DxLib::CheckHitKey(KEY_INPUT_ESCAPE))break; // ESCキーでゲーム終了
-		// ウィンドウに表示されている内容を削除する
-		DxLib::ClearDrawScreen();
-		// 入力状態の取得
-		Input::Instance()->update();
-
+	while (SystemManager::Instance()->processLoop()) {
 		// ↓ここにメイン処理 
 		Scene::Instance()->update();
-
-		 // 描画内容をウィンドウに反映させる
-		DxLib::ScreenFlip();
-
-#ifdef _DEBUG
-		// デバック時:デバック用の表示情報を削除
-		DxLib::clsDx();
-#endif
 	}
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	// 終了処理
+	if (SystemManager::Instance()->finalize() == false)return -1;
 
-	return 0;				// ソフトの終了 
+	return 0;
 }
